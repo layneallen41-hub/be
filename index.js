63276e9d96fbdabfb64c7fd4f7e698493a0814be
layneallen41-hub <layneallen41@gmail.com>
2026-02-05 07:42:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.options('*', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://ftx-settlements.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Max-Age', '3600'); // Optional: Cache preflight response for 1 hour
+  //res.setHeader('Access-Control-Max-Age', '3600'); // Optional: Cache preflight response for 1 hour
   res.status(200).end(); // No content
 });
 
